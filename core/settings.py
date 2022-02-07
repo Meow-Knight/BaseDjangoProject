8c0289c2cfef3a47d69b3346dfe5ccd790cd85e4
GiaBaone <102190252@sv1.dut.udn.vn>
2022-02-07 17:42:39 +0700

    [FIX] delete completely User model

--- core/settings.py
+++ core/settings.py
@@ -177,5 +177,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'api_information.User'
